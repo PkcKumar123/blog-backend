69c1014593f27e6026eb4b6726b26953e7d86795
Prashant <prashantchoubey8338@gmail.com>
2022-09-10 17:07:38 +0530

    first commit

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -24,6 +24,8 @@ export const register = async (req, res) => {
     const options = {
       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
       httpOnly: true,
+      secure: true,
+      sameSite: "none",
     };
 
     res.status(201).cookie("token", token, options).json({
@@ -67,6 +69,8 @@ export const login = async (req, res) => {
     const options = {
       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
       httpOnly: true,
+      secure: true,
+      sameSite: "none",
     };
 
     res.status(200).cookie("token", token, options).json({
